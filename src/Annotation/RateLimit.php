80004e0b8ff5f7a89d72ae866ecf64dcd74bc6bf
luzhe <hsdxgylz@sina.com>
2022-06-12 16:59:10 +0800

    first commit

---
+++ src/Annotation/RateLimit.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\RateLimit\Annotation;
+
+use Attribute;
+use Hyperf\Di\Annotation\AbstractAnnotation;
+
+/**
+ * @Annotation
+ * @Target({"CLASS", "METHOD"})
+ */
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+class RateLimit extends AbstractAnnotation
+{
+    /**
+     * @var int
+     */
+    public $create;
+
+    /**
+     * @var int
+     */
+    public $consume;
+
+    /**
+     * @var int
+     */
+    public $capacity;
+
+    /**
+     * @var null|callable
+     */
+    public $limitCallback;
+
+    /**
+     * @var callable|string
+     */
+    public $key;
+
+    /**
+     * @var int
+     */
+    public $waitTimeout;
+}
